ca8e43b8dc7691581a4ffa862c703440a78154e4
Dmitry <waniz@mail.ru>
2016-11-13 20:28:25 +0300

    test strategy

--- MyStrategy.py
+++ MyStrategy.py
@@ -17,71 +17,126 @@ from model.Unit import Unit
 from model.Wizard import Wizard
 from model.World import World
 
+import random
+
 
 class MyStrategy:
-    # main call
+    # constants section
+    WAYPOINT_RADIUS = 100
+    LOW_HP_FACTOR = 0.25
+
+    # get modules initialised
+    lane = LaneType()
+    waypoints = []
+
     def move(self, me: Wizard, world: World, game: Game, move: Move):
+        self.initialize_strategy(game)
+        self.initialize_tick(world, game, me, move)
 
-        # self.check_top_lane_tower(me=me, world=world, game=game)
-        t1_top_tower = self.get_top_tower_t1_coords(me=me, world=world)
+        # add strafe_speed
+        if self.me.life < self.me.max_life * self.LOW_HP_FACTOR:
+            self.goto(self.get_previous_waypoint())
 
-        # TODO - add check my location and paths for forest
-        my_char = Unit(me.id, me.x, me.y, me.speed_x, me.speed_y, me.angle, me.faction)
-        print('My coordinates: x: %s, y: %s' % (me.x, me.y))
-        print('Target coordinates: x: %s, y: %s' % (t1_top_tower['x'], t1_top_tower['y']))
-        angle_to_target = my_char.get_angle_to(t1_top_tower['x'], t1_top_tower['y'])
-        print('Angle to target: %s' % angle_to_target)
-        if angle_to_target != me.angle:
-            move.turn = angle_to_target - me.angle  #game.wizard_max_turn_angle
 
-        self.go_to_location(me=me, target=t1_top_tower)
 
-        # move.speed = game.wizard_forward_speed
-        # move.strafe_speed = game.wizard_strafe_speed
-        # move.turn = game.wizard_max_turn_angle
-        # move.action = ActionType.MAGIC_MISSILE
+
 
     # ------ helper functions ---------------------------------------
-    # TODO - check if it exists
-    @staticmethod
-    def get_top_tower_t1_coords(me, world):
-        print('Function: get_top_tower_t1_coords')
-        building_list = []
-        print('My fraction: %s' % me.faction)
-        for building in world.buildings:
-            building_list.append([building.x, building.y])
-            print('Building coordinates: x: %s, y: %s. Building type: %s' % (building.x, building.y, building.type))
+    def initialize_strategy(self, game):
+        random.seed(game.random_seed)
+        map_size = game.map_size
+        print('Map size %s' % map_size)
 
-        t1_tower = {'x': building_list[0][0], 'y': building_list[0][1]}
-        if me.faction == Faction.ACADEMY:
-            for pos in range(1, len(building_list)):
-                if t1_tower['y'] > building_list[pos][1]:
-                    t1_tower['x'], t1_tower['y'] = building_list[pos][0], building_list[pos][1]
-        elif me.faction == Faction.RENEGADES:
-            for pos in range(1, len(building_list)):
-                if t1_tower['x'] > building_list[pos][0]:
-                    t1_tower['x'], t1_tower['y'] = building_list[pos][0], building_list[pos][1]
+        # TOP lane waypoints
+        self.waypoints.append([100, map_size - 100])
+        self.waypoints.append([100, map_size - 400])
+        self.waypoints.append([200, map_size - 800])
+        self.waypoints.append([200, map_size * 0.75])
+        self.waypoints.append([200, map_size * 0.5])
+        self.waypoints.append([200, map_size * 0.25])
+        self.waypoints.append([200, 200])
+        self.waypoints.append([map_size * 0.25, 200])
+        self.waypoints.append([map_size * 0.5, 200])
+        self.waypoints.append([map_size * 0.75, 200])
+        self.waypoints.append([map_size - 200, 200])
+        print('Waypoints')
+        print(self.waypoints)
 
-        print('T1 tower is : x %s, y %s' % (t1_tower['x'], t1_tower['y']))
-        print('')
-        return t1_tower
+        self.lane = LaneType.TOP
 
-    @staticmethod
-    def go_to_location(me, target):
-        my_char = Unit(me.id, me.x, me.y, me.speed_x, me.speed_y, me.angle, me.faction)
-        print('Function: go_to_location')
-        print('My coordinates: x: %s, y: %s' % (me.x, me.y))
-        print('Target coordinates: x: %s, y: %s' % (target['x'], target['y']))
-        angle_to_target = my_char.get_angle_to(target['x'], target['y'])
-        print('Angle to target: %s' % angle_to_target)
-        if angle_to_target != me.angle:
-            move.turn = game.wizard_max_turn_angle
+    def initialize_tick(self, world, game, me, move):
+        self.world = world
+        self.game = game
+        self.me = me
+        self.move = move
 
-        print('')
+    def get_next_waypoint(self):
+        last_waypoint_index = len(self.waypoints) - 1
+        last_waypoint = self.waypoints[last_waypoint_index]
 
-    def if_bonus_exists(self, me, world, game):
-        pass
+        for waypoint_index in range(0, last_waypoint_index):
+            waypoint = self.waypoints[waypoint_index]
 
-    # ------ analisys functions -------------------------------------
-    def simulate_next_tick_world(self):
-        pass
+            if self.me.get_distance_to(waypoint) <= self.WAYPOINT_RADIUS:
+                return waypoint[waypoint_index + 1]
+            if last_waypoint.get_distance_to(waypoint) < self.me.get_distance_to(last_waypoint): # 1!!
+                return waypoint
+
+    def get_previous_waypoint(self):
+        first_waypoint = self.waypoints[0]
+        for waypoint_index in range(len(self.waypoints) - 1, 0, -1):
+            waypoint = self.waypoints[waypoint_index]
+            if self.me.get_distance_to(waypoint) <= self.WAYPOINT_RADIUS:
+                return self.waypoints[waypoint_index - 1]
+            if first_waypoint.get_distance_to(first_waypoint) < self.me.get_distance_to(first_waypoint): # 1!!
+                return waypoint
+
+    def goto(self, waypoint):
+        angle = self.me.get_angle_to(waypoint[0], waypoint[1])
+
+        self.move.turn(angle)
+
+        if abs(angle) < self.game.staff_sector / 4:
+            self.move.speed = self.game.wizard_forward_speed
+
+
+    # @staticmethod
+    # def goto_farm_place(me, world, game):
+    #     print('Function: goto_farm_place')
+    #     building_list = []
+    #     for building in world.buildings:
+    #         building_list.append([building.x, building.y])
+    #         print('Building coordinates: x: %s, y: %s. Building type: %s' % (building.x, building.y, building.type))
+    #
+    #     if me.faction == Faction.ACADEMY:
+    #         target_location_t2 = {'x': 50, 'y': 2693}
+    #         target_location_t1 = {'x': 350, 'y': 1656}
+    #
+    #
+    #
+    #
+    #
+    #
+    # @staticmethod
+    # def get_top_tower_t1_coords(me, world):
+    #     print('Function: get_top_tower_t1_coords')
+    #     building_list = []
+    #     print('My fraction: %s' % me.faction)
+    #     for building in world.buildings:
+    #         building_list.append([building.x, building.y])
+    #         print('Building coordinates: x: %s, y: %s. Building type: %s' % (building.x, building.y, building.type))
+    #
+    #     t1_tower = {'x': building_list[0][0], 'y': building_list[0][1]}
+    #     if me.faction == Faction.ACADEMY:
+    #         for pos in range(1, len(building_list)):
+    #             if t1_tower['y'] > building_list[pos][1]:
+    #                 t1_tower['x'], t1_tower['y'] = building_list[pos][0], building_list[pos][1]
+    #     elif me.faction == Faction.RENEGADES:
+    #         for pos in range(1, len(building_list)):
+    #             if t1_tower['x'] > building_list[pos][0]:
+    #                 t1_tower['x'], t1_tower['y'] = building_list[pos][0], building_list[pos][1]
+    #
+    #     print('T1 tower is : x %s, y %s' % (t1_tower['x'], t1_tower['y']))
+    #     print('')
+    #     return t1_tower
+
